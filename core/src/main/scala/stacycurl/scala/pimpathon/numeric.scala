0930aef860b4c63f481bc8086de0c79ef3bf0e52
Stacy Curl <stacy.curl@gmail.com>
2014-03-31 23:24:27 +0100

    Added Numeric.xmap

---
+++ core/src/main/scala/stacycurl/scala/pimpathon/numeric.scala
@@ -0,0 +1,30 @@
+package stacycurl.scala.pimpathon
+
+object numeric {
+  implicit class NumericOps[A](na: Numeric[A]) {
+    def xmap[B](aToB: A => B, bToA: B => A): Numeric[B] = na match {
+      case xna: XMappedNumeric[_, _] => xna.xmap(aToB, bToA)
+      case other                     => new XMappedNumeric[A, B](other, aToB, bToA)
+    }
+  }
+
+  class XMappedNumeric[A, B](na: Numeric[A], aToB: A => B, bToA: B => A) extends Numeric[B] {
+    def xmap[C](bToC: B => C, cToB: C => B): Numeric[C] =
+      new XMappedNumeric[A, C](na, aToB andThen bToC, cToB andThen bToA)
+
+    def compare(l: B, r: B): Int = na.compare(bToA(l), bToA(r))
+
+    def fromInt(i: Int): B = aToB(na.fromInt(i))
+
+    def toDouble(b: B): Double = na.toDouble(bToA(b))
+    def toFloat(b: B): Float   = na.toFloat(bToA(b))
+    def toInt(b: B): Int       = na.toInt(bToA(b))
+    def toLong(b: B): Long     = na.toLong(bToA(b))
+
+    def negate(b: B): B = aToB(na.negate(bToA(b)))
+
+    def minus(l: B, r: B): B = aToB(na.plus(bToA(l), bToA(r)))
+    def plus(l: B, r: B): B = aToB(na.plus(bToA(l), bToA(r)))
+    def times(l: B, r: B): B = aToB(na.plus(bToA(l), bToA(r)))
+  }
+}
---
+++ core/src/test/scala/stacycurl/scala/pimpathon/numeric.scala
@@ -0,0 +1,23 @@
+package stacycurl.scala.pimpathon
+
+import org.junit.Test
+import scalaz.std.list._
+
+import org.junit.Assert._
+import scala.math.Numeric._
+import stacycurl.scala.pimpathon.numeric._
+
+
+class NumericTest {
+  @Test def xmap {
+    val numericString =
+      implicitly[Numeric[Int]].xmap[String](_.toString, Integer.parseInt)
+
+    assertEquals("3", numericString.plus("1", "2"))
+
+    val numericListString =
+      numericString.xmap[List[String]](List(_), _.head)
+
+    assertEquals(List("3"), numericListString.plus(List("1"), List("2")))
+  }
+}

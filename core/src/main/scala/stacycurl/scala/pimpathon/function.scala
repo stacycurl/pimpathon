bcc6960c6bb8a7ec92c6b76ab549a410a2b6fe3b
Stacy Curl <stacy.curl@gmail.com>
2014-04-19 13:50:26 +0100

    Added Predicate.{and, or}

--- core/src/main/scala/stacycurl/scala/pimpathon/function.scala
+++ core/src/main/scala/stacycurl/scala/pimpathon/function.scala
@@ -2,6 +2,9 @@ package stacycurl.scala.pimpathon
 
 object function {
   implicit class PredicateOps[A](val p: A => Boolean) extends AnyVal {
+    def and(q: A => Boolean): (A => Boolean) = (a: A) => (p(a) && q(a))
+    def or(q: A => Boolean):  (A => Boolean) = (a: A) => (p(a) || q(a))
+
     def exists: List[A] => Boolean = (_.exists(p))
     def forall: List[A] => Boolean = (_.forall(p))
   }
--- core/src/test/scala/stacycurl/scala/pimpathon/function.scala
+++ core/src/test/scala/stacycurl/scala/pimpathon/function.scala
@@ -17,5 +17,13 @@ class FunctionTest {
       List(Nil, List(2), List(3), List(2, 4), List(2, 4, 3)).filter(isEven.exists))
   }
 
+  @Test def and {
+    assertEquals(List(4, 6), List(2, 3, 4, 6).filter(isEven and (_ > 2)))
+  }
+
+  @Test def or {
+    assertEquals(List(2, 4, 3), List(2, 1, 4, 3, 5).filter(isEven or (_ == 3)))
+  }
+
   private val isEven: (Int => Boolean) = (_ % 2 == 0)
 }

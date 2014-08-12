b1f8fddac729419fce5d31ad9d1c327b21b16bf0
Sam Halliday <sam.halliday@gmail.com>
2014-08-12 20:21:43 +0100

    close #64 (coverage reports) and work toward #11 (cross builds)

--- project/Build.scala
+++ project/Build.scala
@@ -1,101 +1,36 @@
 import sbt._
 import Keys._
 
-import sbtbuildinfo.Plugin._
-
-import com.typesafe.sbt.SbtGit._
-import GitKeys._
-
-import sbtrelease._
-import sbtrelease.ReleasePlugin._
-import sbtrelease.ReleasePlugin.ReleaseKeys._
-import sbtrelease.ReleaseStateTransformations._
-import sbtrelease.Utilities._
+import net.virtualvoid.sbt.graph.Plugin.graphSettings
+import org.scalastyle.sbt.ScalastylePlugin
+import scoverage.ScoverageSbtPlugin._
+import ScoverageKeys._
+import ScalastylePlugin.{ Settings => scalaStyleSettings }
+import com.typesafe.sbt.SbtScalariform.scalariformSettings
 
 object PimpathonBuild extends Build {
 
   lazy val pimpathon = Project(
-    id = "pimpathon",
+    id = "pimpathon-parent",
     base = file("."),
-    aggregate = Seq(pimpathonCore, pimpathonExamples),
-    settings = commonSettings ++ Seq(
-      moduleName := "pimpathon-root",
-
-      (unmanagedSourceDirectories in Compile) := Nil,
-      (unmanagedSourceDirectories in Test) := Nil,
-
-      publish := (),
-      publishLocal := ()
-    )
+    settings = commonSettings,
+    aggregate = Seq(pimpathonCore, pimpathonExamples)
   )
 
-  lazy val pimpathonCore =
-    Project(
-      id = "pimpathon-core",
-      base = file("core"),
-      settings = commonSettings ++ Publishing.settings ++ buildInfoSettings ++ releaseSettings ++ Seq(
-        moduleName := "pimpathon",
-
-        managedSourceDirectories in Test := Nil,
-
-        libraryDependencies <++= scalaVersion { sv =>
-          Seq(
-            "org.scala-lang" % "scala-compiler" % sv,
-            "com.novocode" % "junit-interface" % "0.7" % "test",
-            "org.scalaz" % "scalaz-core_2.10" % "7.1.0"
-        )},
-
-        initialCommands in console := """import stacycurl.scala.pimpathon._""",
-
-        mappings in (Compile, packageSrc) <++=
-          (sourceManaged in Compile, managedSources in Compile) map { (base, srcs) =>
-            (srcs pair (Path.relativeTo(base) | Path.flat))
-          },
-
-        mappings in (Compile, packageSrc) <++=
-          (mappings in (Compile, packageSrc) in LocalProject("pimpathon-examples")),
-
-        buildInfoPackage := "pimpathon",
-        buildInfoKeys := Seq[BuildInfoKey](version, scalaVersion),
-        buildInfoKeys ++= Seq[BuildInfoKey](
-          version,
-          scalaVersion,
-          gitHeadCommit,
-          BuildInfoKey.action("buildTime") {
-            System.currentTimeMillis
-          }
-        ),
-
-        releaseProcess := Seq[ReleaseStep](
-          checkSnapshotDependencies,
-          inquireVersions,
-          runTest,
-          setReleaseVersion,
-          commitReleaseVersion,
-          tagRelease,
-          setNextVersion,
-          commitNextVersion,
-          pushChanges
-        )
-      )
+  lazy val pimpathonCore = Project(
+    id = "pimpathon-core",
+    base = file("core"),
+    settings = commonSettings ++ Seq(
+      libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.0"
     )
+  )
 
   lazy val pimpathonExamples = Project(
     id = "pimpathon-examples",
     base = file("examples"),
     dependencies = Seq(pimpathonCore),
-
     settings = commonSettings ++ Seq(
-      libraryDependencies <++= scalaVersion { sv =>
-        Seq(
-          "org.scala-lang" % "scala-compiler" % sv,
-          "com.novocode" % "junit-interface" % "0.7" % "test"
-      )},
-
-      runAllIn(Compile),
-
-      publish := (),
-      publishLocal := ()
+      runAllIn(Compile)
     )
   )
 
@@ -107,21 +42,26 @@ object PimpathonBuild extends Build {
     }
   }
 
-  def commonSettings = Defaults.defaultSettings ++
-    Seq(
-      organization        := "com.github.stacycurl",
-      scalaVersion        := "2.10.3",
-      scalaBinaryVersion  := "2.10.3",
-
-      (unmanagedSourceDirectories in Compile) <<= (scalaSource in Compile)(Seq(_)),
-      (unmanagedSourceDirectories in Test) <<= (scalaSource in Test)(Seq(_)),
-
-      scalacOptions       := Seq(
-        "-feature",
-        "-language:higherKinds",
-        "-language:implicitConversions",
-        "-Xfatal-warnings",
-        "-deprecation",
-        "-unchecked")
-    )
+  def commonSettings = graphSettings ++
+  // uncomment when you want to reset the formatting of the project
+  // scalariformSettings ++
+  scalaStyleSettings ++ instrumentSettings ++ Seq(
+    organization := "com.github.stacycurl",
+    scalaVersion := "2.11.2",
+    maxErrors := 1,
+    parallelExecution in Test := true,
+    scalacOptions       := Seq(
+      "-feature",
+      "-language:higherKinds",
+      "-language:implicitConversions",
+      "-Xfatal-warnings",
+      "-deprecation",
+      "-unchecked"
+    ),
+    libraryDependencies += "com.novocode" % "junit-interface" % "0.10" % "test",
+    initialCommands in console := """import stacycurl.scala.pimpathon._""",
+    minimumCoverage := 85,
+    highlighting := true,
+    failOnMinimumCoverage := true
+  )
 }

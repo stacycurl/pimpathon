212777933494aa0dee3e603ad37b98a4edacf9f1
Stacy Curl <stacy.curl@gmail.com>
2014-08-14 20:43:34 +0100

    Added OutputStream.closeIf

--- core/src/main/scala/pimpathon/java/io/OutputStream.scala
+++ core/src/main/scala/pimpathon/java/io/OutputStream.scala
@@ -3,11 +3,13 @@ package pimpathon.java.io
 import java.io.{InputStream, OutputStream}
 import scala.annotation.tailrec
 
+import pimpathon.any._
 import pimpathon.java.io.inputStream._
 
 
 object outputStream {
   implicit class OutputStreamOps(val os: OutputStream) extends AnyVal {
     def write(is: InputStream): OutputStream = { is.read(os); os }
+    def closeIf(condition: Boolean): OutputStream = os.tapIf(_ => condition)(_.close)
   }
 }
--- core/src/test/scala/pimpathon/java/io/OutputStream.scala
+++ core/src/test/scala/pimpathon/java/io/OutputStream.scala
@@ -5,9 +5,21 @@ import org.junit.Test
 
 import org.junit.Assert._
 import pimpathon.java.io.outputStream._
+import pimpathon.util._
 
 
 class OutputStreamTest {
+  @Test def closeIf {
+    val os = createOutputStream()
+    assertInputStreamClosed(false, os.closed)
+
+    os.closeIf(false)
+    assertInputStreamClosed(false, os.closed)
+
+    os.closeIf(true)
+    assertInputStreamClosed(true, os.closed)
+  }
+
   @Test def write {
     def assertWrite(input: String): Unit = {
       val is: InputStream = new ByteArrayInputStream(input.getBytes)

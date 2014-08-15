6f21c8b7d459e86e2b51f70347ae556c4cbe264a
Stacy Curl <stacy.curl@gmail.com>
2014-08-15 21:28:50 +0100

    Added Tuple2.to

--- core/src/main/scala/pimpathon/tuple.scala
+++ core/src/main/scala/pimpathon/tuple.scala
@@ -4,5 +4,6 @@ package pimpathon
 object tuple {
   implicit class Tuple2Ops[A, B](val t: (A, B)) extends AnyVal {
     def calc[C](f: (A, B) => C): C = f(t._1, t._2)
+    def to[C](implicit ac: A => C, bc: B => C): (C, C) = (ac(t._1), bc(t._2))
   }
 }
--- core/src/test/scala/pimpathon/tuple.scala
+++ core/src/test/scala/pimpathon/tuple.scala
@@ -10,4 +10,11 @@ class TupleTest {
   @Test def calc {
     assertEquals("123abc", ("123", "abc").calc(_ + _))
   }
+
+  @Test def to {
+    implicit def intToString(i: Int): String = i.toString
+    implicit def doubleToString(d: Double): String = d.toString
+
+    assertEquals(("123", "456.0"), (123, 456.0).to[String])
+  }
 }

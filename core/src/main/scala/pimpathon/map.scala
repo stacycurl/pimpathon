0ff9be4b5de794771f54f86d2439038b340dea42
Stacy Curl <stacy.curl@gmail.com>
2014-08-15 14:32:00 +0100

    Added Map[K, V].containsAny, closes #35

--- core/src/main/scala/pimpathon/map.scala
+++ core/src/main/scala/pimpathon/map.scala
@@ -1,10 +1,15 @@
 package pimpathon
 
+import scala.collection.GenTraversableOnce
+
 import pimpathon.multiMap._
 
 
 object map {
   implicit class MapOps[K, V](val map: Map[K, V]) extends AnyVal {
+    def containsAny(ok: Option[K]): Boolean = ok.fold(false)(map.contains)
+    def containsAny[GK <: GenTraversableOnce[K]](gk: GK): Boolean = gk.exists(map.contains)
+
     def get(ok: Option[K]): Option[V] = ok.flatMap(map.get)
 
     def getOrThrow(k: K, message: String): V =

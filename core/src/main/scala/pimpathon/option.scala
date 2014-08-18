ac0cee58c81cba5525f374c1f11449003cbfa304
Stacy Curl <stacy.curl@gmail.com>
2014-08-18 17:59:56 +0100

    Pass Exception by name in Option.getOrThrow

--- core/src/main/scala/pimpathon/option.scala
+++ core/src/main/scala/pimpathon/option.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 object option {
   implicit class OptionOps[A](val option: Option[A]) extends AnyVal {
     def getOrThrow(message: String): A = getOrThrow(new NoSuchElementException(message))
-    def getOrThrow(exception: Exception): A = option.getOrElse(throw exception)
+    def getOrThrow(exception: => Exception): A = option.getOrElse(throw exception)
     def toTry: Try[A] = option.fold(pimpTry.failure[A](new NoSuchElementException))(pimpTry.success[A](_))
   }
 }

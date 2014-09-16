b6877e513cf3ab14fd8cabbcaba73a944a2a2f14
Stacy Curl <stacy.curl@gmail.com>
2014-09-16 19:13:52 +0100

    Added Array[Byte].readUpToN

--- core/src/main/scala/pimpathon/array.scala
+++ core/src/main/scala/pimpathon/array.scala
@@ -1,6 +1,8 @@
 package pimpathon
 
+import _root_.java.io.{InputStream, OutputStream}
 import _root_.java.math.BigInteger
+import scala.math
 
 import pimpathon.string._
 
@@ -9,6 +11,9 @@ object array {
   implicit class ByteArrayOps(val array: Array[Byte]) extends AnyVal {
     def toHex(length: Int): String = toHex.prefixPadTo(length, '0')
     def toHex: String = new BigInteger(1, array).toString(16)
+
+    def readUpToN(n: Long, is: InputStream): Int =
+      if (n == 0) -1 else is.read(array, 0, math.min(n, array.length).toInt)
   }
 }
 
--- core/src/test/scala/pimpathon/array.scala
+++ core/src/test/scala/pimpathon/array.scala
@@ -4,11 +4,25 @@ import org.junit.Test
 
 import org.junit.Assert._
 import pimpathon.array._
+import pimpathon.util._
 
 
 class ArrayTest {
   @Test def toHex: Unit = {
     assertEquals("7e57ab1e", Array[Byte](126, 87, -85, 30).toHex)
+  }
+
+  @Test def readUpToN: Unit = {
+    def read(input: String, n: Int, bufferSize: Int): (String, Int) = {
+      val buffer = new Array[Byte](bufferSize)
+      val count = buffer.readUpToN(n, createInputStream(input))
+
+      (new String(buffer).substring(0, count), count)
+    }
+
+    assertEquals(("contents", 8), read("contents", 9, 8))
+    assertEquals(("contents", 8), read("contents", 8, 8))
+    assertEquals(("content",  7), read("contents", 7, 8))
   }
 }
 

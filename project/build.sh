35b8948f65354d68f3d7d1d3996cec1cdf8f30c6
Stacy Curl <stacy.curl@gmail.com>
2016-03-18 18:17:52 +0000

    Added build.sh

---
+++ project/build.sh
@@ -0,0 +1,13 @@
+i#!/bin/bash
+
+RANGE=$(echo ${TRAVIS_COMMIT_RANGE:-HEAD} | sed -e 's/\.\.\./../g')
+CHANGES=$(git diff-tree -r $RANGE)
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$CHANGES" == *"version.sbt"* ]]; then
+  PUBLISH="publish"
+else
+  PUBLISH=""
+fi
+
+echo sbt clean compile test $PUBLISH
+sbt clean compile test $PUBLISH

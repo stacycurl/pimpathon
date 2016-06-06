9219f783f8d32cf7dcbca169e012b07d669a553b
Stacy Curl <stacy.curl@gmail.com>
2016-06-06 16:44:22 +0100

    Only publish when version.sbt has changed but not for pull requests

--- project/build.sh
+++ project/build.sh
@@ -9,7 +9,7 @@ echo "CHANGES: $CHANGES"
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$CHANGES" == *"version.sbt"* ]]; then
   PUBLISH="publish"
 else
-  PUBLISH="publish"
+  PUBLISH=""
 fi
 
 echo sbt clean compile test $PUBLISH

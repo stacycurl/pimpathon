4c8f148d74438256d78ad679d0f5c9de0e0fde13
Stacy Curl <stacy.curl@gmail.com>
2020-04-14 00:23:04 +0100

    Added Properties.propEnvOrElse(String, String): String

--- src/main/scala/pimpathon/properties.scala
+++ src/main/scala/pimpathon/properties.scala
@@ -5,6 +5,7 @@ import scala.util.Properties
 
 object properties {
   implicit class PropertiesCompanionPimps(val self: Properties.type) extends AnyVal {
+    def propEnvOrElse(key: String, alt: String): String = propEnvOrNone(key).getOrElse(alt)
     def propEnvOrNone(key: String): Option[String] = Properties.propOrNone(key).orElse(Properties.envOrNone(key))
   }
 }

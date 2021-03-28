766461351eeb8bf62fbcc16264a2258fc7ed7f1f
Stacy Curl <stacy.curl@gmail.com>
2021-03-28 23:53:31 +0100

    Added AtomicReference[A].updateEither(A => Either[L, A]): Either[L, A]

--- src/main/scala/pimpathon/java/util/concurrent/atomic/AtomicReference.scala
+++ src/main/scala/pimpathon/java/util/concurrent/atomic/AtomicReference.scala
@@ -1,10 +1,12 @@
 package pimpathon.java.util.concurrent.atomic
 
 import java.util.concurrent.atomic.AtomicReference
+import pimpathon.either.EitherPimps
 
 
 object atomicReference {
   implicit class AtomicReferencePimps[A](private val self: AtomicReference[A]) extends AnyVal {
     def update(f: A ⇒ A): A = self.updateAndGet((a: A) ⇒ f(a))
+    def updateEither[L](f: A ⇒ Either[L, A]): Either[L, A] = f(self.get()).tapRight(self.set)
   }  
 }
--- src/test/scala/pimpathon/java/util/concurrent/atomic/AtomicReferenceTest.scala
+++ src/test/scala/pimpathon/java/util/concurrent/atomic/AtomicReferenceTest.scala
@@ -9,4 +9,9 @@ import pimpathon.util._
 class AtomicReferenceTest {
   @Test def update(): Unit = 
     (new AtomicReference(123) |> (ref => (ref.update(_ * 2), ref.get()))) === (246, 246)
+
+  @Test def updateEither(): Unit = {
+    (new AtomicReference(123) |> (ref => (ref.updateEither(i => Left(i * 2)),  ref.get()))) === (Left(246),  123)
+    (new AtomicReference(123) |> (ref => (ref.updateEither(i => Right(i * 2)), ref.get()))) === (Right(246), 246)
+  }
 }
